55120b55fd2030ed656851748705e4887238a55c
Arvinnn2 <arvin.hadiyyatullah.3@gmail.com>
2025-05-10 00:07:50 +0700

    Create data.js

---
+++ pages/data.js
@@ -0,0 +1,38 @@
+let books = [
+  {
+    "id": 1746707769787,
+    "title": "BORNPINK",
+    "author": "BLACKPINK"
+  },
+  {
+    "id": 1746708049515,
+    "title": "SUPERNATURAL",
+    "author": "NewJeans"
+  },
+  {
+    "id": 1746708721805,
+    "title": "ROSIE",
+    "author": "Rosé"
+  },
+  {
+    "id": 1746709671886,
+    "title": "Vogue ",
+    "author": "Anna"
+  },
+  {
+    "id": 1746762710090,
+    "title": "LIKE JENNIE",
+    "author": "JENNIE"
+  },
+  {
+    "id": 1746791278540,
+    "title": "Horror",
+    "author": "Jurnalisa"
+  },
+  {
+    "id": 1746802043066,
+    "title": "Horror",
+    "author": "Nadia Omara"
+  }
+];
+module.exports = { books };
